26c6057a53d41a15579604ab0bbf407bf370c042
Ismail Magomedov <ismail@variant.net>
2023-11-18 18:35:30 +0400

    New homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,26 +2,24 @@
 	import '$lib/styles/fonts.css';
 	import '$lib/styles/colors.css';
 	import '$lib/styles/global.css';
-	import Container from '$lib/components/Container.svelte';
 </script>
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&family=Caprasimo:wght@400&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@100..900&&family=Rubik+Mono+One&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
 
 <main class="app">
-	<Container>
-		<slot />
-	</Container>
+	<slot />
 </main>
 
 <style>
 	main {
+		font-family: 'Azeret Mono', monospace;
 		height: 100vh;
 		background: var(--blue-gray-100);
 	}
---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@100..900&display=swap"
+		rel="stylesheet"
+	/>
+	
+</svelte:head>
+
+<slot />
