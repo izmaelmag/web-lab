178774e29574e004c4fefcf8243a3829532feba3
Ismail Magomedov <ismail@variant.net>
2023-12-14 00:47:26 +0400

    Update lins styles

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -6,7 +6,29 @@
 <Layout>
 	<Header links={[{ title: 'Playground' }]} />
 
-	<a href="/playground/spirograph">Spirograph</a>
-	<a href="/playground/loop_demo">Loop demo</a>
-	<a href="/playground/daily-1">Daily #1</a>
+	<nav>
+		<a href="/playground/spirograph">Spirograph</a>
+		<a href="/playground/loop_demo">Loop demo</a>
+		<a href="/playground/daily-1">Daily #1</a>
+	</nav>
 </Layout>
+
+<style>
+	nav {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+	}
+
+	a {
+		color: var(--blue-gray-600);
+		text-decoration: none;
+		font-size: 32px;
+		line-height: 1;
+		font-weight: 900;
+
+		&:hover {
+			color: var(--orange-600);
+		}
+	}
+</style>
