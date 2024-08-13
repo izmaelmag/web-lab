abb14099e1a2dd9b448d8c40e72405b27fdb29f5
Ismail Magomedov <izmaelmag@pm.me>
2024-08-13 20:42:31 +0400

    Tailwind and shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,28 @@
-<script>
-	import { SvelteUIProvider } from '@svelteuidev/core';
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+<script>import "../app.css";
+import { SvelteUIProvider } from '@svelteuidev/core';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
-	import '$lib/styles/fonts.css';
-	import '$lib/styles/colors.css';
-	import '$lib/styles/global.css';
-	import { onMount } from 'svelte';
+import '$lib/styles/fonts.css';
+import '$lib/styles/colors.css';
+import '$lib/styles/global.css';
+import { onMount } from 'svelte';
 
-	onMount(() => {
-		injectSpeedInsights();
-	});
-</script>
+onMount(() => {
+    injectSpeedInsights();
+});</script>
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@100..900&display=swap"
-		rel="stylesheet"
-	/>
+	<link href="https://fonts.googleapis.com/css2?family=Azeret+Mono:wght@100..900&amp;display=swap" rel="stylesheet" />
 </svelte:head>
 
 <SvelteUIProvider>
 	<main class="app">
-		<slot />
+		<slot></slot>
 	</main>
 </SvelteUIProvider>
+
 
 <style>
 	main {
