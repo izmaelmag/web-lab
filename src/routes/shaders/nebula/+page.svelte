7ac39cd451f382040417830bb1fe0ca70ec5a574
izmaelmag <caspian.seagull@gmail.com>
2025-02-02 21:33:36 +0400

    Better nebula shader

---
+++ src/routes/shaders/nebula/+page.svelte
@@ -0,0 +1,27 @@
+<div class="fullscreenContainer">
+  <iframe
+    src="/glsl/nebula/index.html"
+    title="nebula"
+    class="fullscreen"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</div>
+
+<style>
+  .fullscreen {
+    width: 100vw;
+    height: 100vh;
+    border: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+
+  .fullscreenContainer {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+  }
+</style>
